e4a5b662bcff8a77e3bbdcd3031fd2bee9b618a6
bangbang.song <bangbang.song@6c3c5979-8dc6-f465-5e97-4051e158785d>
2010-11-29 07:10:33 +0000

    type error.

--- trunk/android/view/CustomAtt/src/com/bangbangsong/test/TestView.java
+++ trunk/android/view/CustomAtt/src/com/bangbangsong/test/TestView.java
@@ -3,16 +3,18 @@ package com.bangbangsong.test;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.TextView;
 
 public class TestView extends TextView {
+	private static final String TAG = TestView.class.getSimpleName();
 	
 	public TestView(Context context){
 		this(context, null);
 	}
 
 	public TestView(Context context, AttributeSet attrs) {
-		this(context, attrs, 0);
+		this(context, attrs, R.attr.ooxx);
 		// TODO Auto-generated constructor stub
 	}
 	
@@ -20,6 +22,8 @@ public class TestView extends TextView {
 		super(context, attrs, defStyle);
 		TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TestView, defStyle, 0);
 		String mystr = a.getString(R.styleable.TestView_ooxx);
+		
+		Log.d(TAG, "custom string: " + mystr);
 	}
 
 }
