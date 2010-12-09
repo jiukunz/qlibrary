626febf0279af41c862a51761268d18c0254528e
bangbang.song <bangbang.song@6c3c5979-8dc6-f465-5e97-4051e158785d>
2010-12-09 11:32:50 +0000

    add verionCode & versionName.

--- android/package/FileMan/src/org/bangbang/song/android/fileman/activity/AboutActivity.java
+++ android/package/FileMan/src/org/bangbang/song/android/fileman/activity/AboutActivity.java
@@ -3,7 +3,11 @@ package org.bangbang.song.android.fileman.activity;
 import org.bangbang.song.android.fileman.R;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutActivity extends Activity {
 	@Override
@@ -13,5 +17,18 @@ public class AboutActivity extends Activity {
 		
 		setContentView(R.layout.about);
 		setTheme(android.R.style.Theme_Dialog);
+		
+		PackageInfo info;
+		try {
+			info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_META_DATA);
+
+			int versionCode = info.versionCode;
+			String versionName = info.versionName;
+			String about = String.format(getString(R.string.about), versionCode, versionName);
+			((TextView)findViewById(R.id.text)).setText(about);
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		};
 	}
 }
