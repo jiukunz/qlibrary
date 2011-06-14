c743dfd38b8587b76d8012190eb2b831b567e0d4
bangbang.song <bangbang.song@gmail.com>
2011-06-14 09:27:51 +0000

    format code

--- android/package/commonlib/src/org/bangbang/song/andorid/common/debug/Log.java
+++ android/package/commonlib/src/org/bangbang/song/andorid/common/debug/Log.java
@@ -1,33 +1,32 @@
+
 package org.bangbang.song.andorid.common.debug;
 
 import java.io.File;
 
 import android.util.*;
+
 /**
- * @author bangbang.song@gmail.com
- * 
- * JUST a wrapper for android.util.Log
- *
+ * @author bangbang.song@gmail.com JUST a wrapper for android.util.Log
  */
 public class Log {
 
-	public static void d(String tag, String string) {
-		// TODO Auto-generated method stub
-		android.util.Log.d(tag, string);
-	}
+    public static void d(String tag, String string) {
+        // TODO Auto-generated method stub
+        android.util.Log.d(tag, string);
+    }
 
-	public static void e(String tag, String message) {
-		// TODO Auto-generated method stub
-		android.util.Log.d(tag, message);
-	}
+    public static void e(String tag, String message) {
+        // TODO Auto-generated method stub
+        android.util.Log.d(tag, message);
+    }
 
-	public static void init(File appExternalLogDir, String string) {
-		// TODO Auto-generated method stub
-		
-	}
+    public static void init(File appExternalLogDir, String string) {
+        // TODO Auto-generated method stub
 
-	public static void w(String tag, String string) {
-		// TODO Auto-generated method stub
-		android.util.Log.w(tag, string);
-	} 
+    }
+
+    public static void w(String tag, String string) {
+        // TODO Auto-generated method stub
+        android.util.Log.w(tag, string);
+    }
 }
