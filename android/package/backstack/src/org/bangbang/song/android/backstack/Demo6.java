8f5eb62ac9d5ec44e5008ea82ee39e99c7794093
bangbang.song <bangbang.song@gmail.com>
2011-08-22 04:35:22 +0000

    initial import.

---
+++ android/package/backstack/src/org/bangbang/song/android/backstack/Demo6.java
@@ -0,0 +1,23 @@
+package org.bangbang.song.android.backstack;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Demo6 extends TracingLifeCycleActivity {
+	private static final String TAG = Demo6.class.getSimpleName();
+	
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+
+	@Override
+	protected String getTag() {
+		// TODO Auto-generated method stub
+		return TAG;
+	}
+}
