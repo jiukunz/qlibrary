6bdf58d4aa85249de282479dbac2804a97b454ca
bangbang.song@gmail.com <bangbang.song@gmail.com@6c3c5979-8dc6-f465-5e97-4051e158785d>
2011-11-11 00:55:31 +0000

    more log.

--- trunk/android/package/BroadGalaxy/src/com/broadgalaxy/bluz/Application.java
+++ trunk/android/package/BroadGalaxy/src/com/broadgalaxy/bluz/Application.java
@@ -1,16 +1,23 @@
 package com.broadgalaxy.bluz;
 
+import java.io.File;
+
+import android.os.Environment;
+
 import com.broadgalaxy.util.Log;
 
 public class Application extends android.app.Application {
     public static final String PREF_FILE_NAME= "pref_user";
     public static final String PREF_USER_ID = "pref_user_id";
     private static final String TAG = Application.class.getSimpleName();
+    private File appExternalLogDir = new File(Environment.getExternalStorageDirectory(), "broadgalaxy");
     
     @Override
     public void onCreate() {
         super.onCreate();
         
+        Log.setLog2File(true);
+        Log.init(appExternalLogDir );
         Log.d(TAG , "hi world");
     }
 
