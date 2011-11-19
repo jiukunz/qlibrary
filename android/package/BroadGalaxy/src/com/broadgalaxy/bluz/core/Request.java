5263476301c6c2e63e64ea268f1f1bccabeeeb38
bangbang.song <bangbang.song@gmail.com>
2011-11-19 06:01:03 +0000

    add len.

--- android/package/BroadGalaxy/src/com/broadgalaxy/bluz/core/Request.java
+++ android/package/BroadGalaxy/src/com/broadgalaxy/bluz/core/Request.java
@@ -2,7 +2,10 @@ package com.broadgalaxy.bluz.core;
 
 import java.util.zip.CRC32;
 
-abstract public class Request extends Pack {
+import com.broadgalaxy.util.Log;
+
+public class Request extends Pack {
+    String TAG = Request.class.getSimpleName();
 
     public Request(String code, int address, byte[] payload) {
         super();
@@ -19,8 +22,27 @@ abstract public class Request extends Pack {
 
     private int genCRC() {
         CRC32 crc = new CRC32();
+        byte c = 0;
+        byte[] nonCrcBytes = getNonCRCByte();
+        boolean first = true;
+        for (byte b : nonCrcBytes) {
+            if (first) {
+                first = !first;
+                c = b;
+            } else {
+                c ^= b;
+            }
+        }
+        
+        if (true) {
+            return c;
+        }
         crc.update(getNonCRCByte());
         
+        long crcV = crc.getValue();
+        Log.d(TAG, "crcV: " + crcV);
+        Log.d(TAG, "binary: " + Long.toHexString(crcV));
+//        Log.d(TAG, "binary: " + crcV)
         return (int)crc.getValue();
     }
 }
