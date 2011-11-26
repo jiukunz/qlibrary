2baafa52c4ec8de5331b0cab588ad33f7b0a8ae1
bangbang.song <bangbang.song@gmail.com>
2011-11-26 12:03:03 +0000

    ui adjusting.

--- android/package/BroadGalaxy/src/com/broadgalaxy/bluz/activity/ChatActivity.java
+++ android/package/BroadGalaxy/src/com/broadgalaxy/bluz/activity/ChatActivity.java
@@ -173,8 +173,6 @@ public class ChatActivity extends BluzActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (D)
-            Log.e(TAG, "+++ ON CREATE +++");
 
         // Set up the window layout
         requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
